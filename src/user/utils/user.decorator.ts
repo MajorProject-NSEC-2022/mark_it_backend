fc5f518005f8ca41a51d28e1a93591e0e935e02e
AritroSinha <sinhaaritro@gmail.com>
2022-04-27 14:03:15 +0530

    Added GetUserID Decorator

--- src/user/utils/user.decorator.ts
+++ src/user/utils/user.decorator.ts
@@ -10,3 +10,10 @@ export const GetUser = createParamDecorator(
     return request.user;
   },
 );
+
+export const GetUserID = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.userID;
+  },
+);
